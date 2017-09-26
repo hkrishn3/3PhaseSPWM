8ac0a18033af48973e8c529b0576589c9fb7f3f8
hkrishn3 <32269884+hkrishn3@users.noreply.github.com>
2017-09-26 11:29:01 -0400

    Add files via upload

---
+++ library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 9/26/2017 11:10:30 AM
+ * Author : hkrishnan
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
